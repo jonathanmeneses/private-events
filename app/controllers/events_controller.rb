72b388a2f84e8b656fa66731cc8b9123ea299cec
Jonathan Meneses <jonathan.m.meneses@gmail.com>
2024-03-02 19:12:07 -0600

    add form partials and begin new/edits

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,4 +2,27 @@ class EventsController < ApplicationController
   def index
     @events = Event.all
   end
+
+  def new
+
+    @event = current_user.events.new
+
+  end
+
+  def create
+    @event = event.new(event_params)
+
+    if @event.save
+      redirect_to root_path, notice: "Event Successfully Created"
+    else
+      render :new, status: :unprocessable_entity
+    end
+
+  end
+
+  private
+  def event_params
+    params.require(:event).permit(:title, :description, :date, :location)
+  end
+
 end

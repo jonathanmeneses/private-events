2c83b5380b70293e346443b682e7b877969b05ed
Jonathan Meneses <jonathan.m.meneses@gmail.com>
2024-03-02 18:54:39 -0600

    Comments in routing

---
+++ config/routes.rb
@@ -0,0 +1,19 @@
+Rails.application.routes.draw do
+  devise_for :users
+
+  #Putting this after the devise routes to give devise precedences
+  resources :users, only: [:show] do
+    resources :events, only: [:index] # This will generate a route for showing events associated with a user
+  end
+  get 'events/index'
+
+  root 'events#index'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
+  # Can be used by load balancers and uptime monitors to verify that the app is live.
+  get "up" => "rails/health#show", as: :rails_health_check
+
+  # Defines the root path route ("/")
+  # root "posts#index"
+end

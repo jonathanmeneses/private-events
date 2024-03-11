21bccb63987760e299673c76bd9e42eae0d0959a
Jonathan Meneses <jonathan.m.meneses@gmail.com>
2024-03-10 23:00:19 -0500

    Work on User Controller redirects

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,16 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!, only: [:index]
   def show
-    @user = User.find(params[:id])
+    puts current_user.id.to_i ==  params[:id].to_i
+    if params[:id].to_i == current_user.id
+      redirect_to users_path
+    else
+      @user = User.find(params[:id])
+      @future_public_or_invited_user_events = @user.created_events.future.open_invite
+      @user_events_invited_current_user = @user.created_events.attended_by_user(current_user.id).or(@user.created_events.open_invite)
+    # past user invited or visible events @user_events_invited_current_user.past.inspect
+    # past user invited or visible events @user_events_invited_current_user.past.inspect
+    end
   end
 
   def index
--- app/models/event.rb
+++ app/models/event.rb
@@ -6,4 +6,9 @@ class Event < ApplicationRecord
 
   scope :past, -> {where("date < ?",DateTime.now)}
   scope :future, -> {where("date > ?", DateTime.now)}
+  scope :by_creator, ->(creator_id) { where(creator_id: creator_id) }
+  scope :open_invite, -> {where(invite_only: [false,nil])}
+  # .or(where(invite_only: nil))}
+  scope :invite_only, -> {where(invite_only: true)}
+  scope :attended_by_user, ->(user_id) {Event.where(id: Attendance.select(:attended_event_id).where(attendee_id: user_id))}
 end

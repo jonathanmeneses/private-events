aa885f508f65912fe0651dff846632051a984b10
Jonathan Meneses <jonathan.m.meneses@gmail.com>
2024-03-14 23:59:29 -0500

    Add Attendee Views

--- db/migrate/20240303000818_add_description_to_events.rb
+++
@@ -1,5 +0,0 @@
-class AddDescriptionToEvents < ActiveRecord::Migration[7.1]
-  def change
-    add_column :events, :description, :text
-  end
-end
--- db/migrate/20240310143313_add_invite_only_to_events.rb
+++
@@ -1,5 +0,0 @@
-class AddInviteOnlyToEvents < ActiveRecord::Migration[7.1]
-  def change
-    add_column :events, :invite_only, :boolean, default: false
-  end
-end

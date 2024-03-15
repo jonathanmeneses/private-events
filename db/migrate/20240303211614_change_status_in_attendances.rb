584b9451e568511df3d63dd5b547cc1785245f36
Jonathan Meneses <jonathan.m.meneses@gmail.com>
2024-03-14 23:59:01 -0500

    Add Attendee Count Views

--- db/migrate/20240303211614_change_status_in_attendances.rb
+++
@@ -1,9 +0,0 @@
-class ChangeStatusInAttendances < ActiveRecord::Migration[7.1]
-  def up
-    change_column :attendances, :status, 'integer USING CAST(status AS integer)'
-  end
-
-  def down
-    change_column :attendances, :status, :string
-  end
-end
